fb8b3de510a3a24c66930469990e71a170f5072e
Fawaz Joseph <AlagidiOmokunrin@gmail.com>
2023-04-04 20:16:31 -0400

    Updating minimum plugin to use latest constructor

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
